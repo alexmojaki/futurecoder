96047417d148cf5a5cc8b208dbbd0e367c091382
Alex Hall <alex.mojaki@gmail.com>
2020-08-08 17:13:46 +0200

    Remove password validation

--- backend/book/settings.py
+++ backend/book/settings.py
@@ -98,24 +98,6 @@ DATABASES = {
     'default': dj_database_url.config(default="sqlite:///db.sqlite3")
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
