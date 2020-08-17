75d07781d1726db6b20d09da2c68e8bae1573013
Alex Hall <alex.mojaki@gmail.com>
2020-08-17 21:25:47 +0200

    WIP styling the home page

--- backend/book/settings.py
+++ backend/book/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static_backend/"
 STATICFILES_DIRS = [
     str(BASE_DIR.parent / f"frontend/build/static")
 ]
