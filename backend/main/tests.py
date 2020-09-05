607183230fdf1d11aa0321fbb42e0b6e64de4bd3
Alex Hall <alex.mojaki@gmail.com>
2020-09-05 13:56:20 +0200

    Fix get_solution for non-function exercise, add tests

--- backend/main/tests.py
+++ backend/main/tests.py
@@ -47,26 +47,37 @@ class StepsTestCase(TestCase):
                     for line in response["result"]:
                         line["text"] = normalise_output(line["text"])
                     del response["birdseye_url"]
-                    transcript.append(dict(
+                    transcript_item = dict(
                         program=program.splitlines(),
                         page=page.title,
                         step=step_name,
                         response=response,
-                    ))
+                    )
+                    transcript.append(transcript_item)
                     is_message = substep in step.messages
                     if is_message:
-                        self.assertEqual(response["message"], substep.text)
-
+                        self.assertEqual(response["message"], substep.text, transcript_item)
+                    elif step.hints:
+                        solution_response = api(
+                            "get_solution",
+                            page_index=page_index,
+                            step_index=step_index,
+                        )
+                        get_solution = "".join(solution_response["tokens"])
+                        assert "def solution(" not in get_solution
+                        assert "returns_stdout" not in get_solution
+                        assert get_solution.strip() in program
+                        transcript_item["get_solution"] = get_solution.splitlines()
                     self.assertEqual(
                         response["passed"],
                         not is_message,
-                        transcript[-1],
+                        transcript_item,
                     )
 
                     self.assertEqual(
                         step_index + response["passed"],
                         state["pages_progress"][page_index],
-                        transcript[-1],
+                        transcript_item,
                     )
         path = Path(__file__).parent / "test_transcript.json"
         if os.environ.get("FIX_TESTS", 0):
