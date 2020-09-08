e61909fe30efb4ddfedde8c7491dff6eb04b6db0
spamegg <4255997+spamegg1@users.noreply.github.com>
2020-09-08 07:36:06 +0300

    Update backend/main/chapters/c09_combining_booleans.py
    committed suggestion on line 176
    
    Co-authored-by: Alex Hall <alex.mojaki@gmail.com>

--- backend/main/chapters/c09_combining_booleans.py
+++ backend/main/chapters/c09_combining_booleans.py
@@ -173,7 +173,7 @@ Replace the `return` line in the code with the above line, and try running it ag
         text = """
 The second and third tests fail! Our function seems to be doing the wrong thing:
 it returns `"Bob"` (a string, not a boolean!) when `name` is `"Bob"` or `"Charlie"`. What is going on?
-Try inspecting the code with Birdseye. Inspect the `return` statements of each `is_friend` calls carefully.
+Try inspecting the code with Bird's Eye. Inspect the `return` statements of each `is_friend` call carefully.
 (Use the blue arrow buttons)
         """
 
