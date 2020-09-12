4c46898703da5a0f042aac958ca1c9062a99f87d
Alex Hall <alex.mojaki@gmail.com>
2020-09-12 14:54:05 +0200

    Increase ace font size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -205,6 +205,7 @@ class AppComponent extends React.Component {
                 editor.renderer.setScrollMargin(10);
                 editor.renderer.setPadding(10);
               }}
+              fontSize="12pt"
             />
           </div>
           <div className="terminal">
