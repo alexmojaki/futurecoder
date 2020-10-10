8a5c2a05941465c4c0852e515e05b469cc803c1a
Alex Hall <alex.mojaki@gmail.com>
2020-10-10 22:05:10 +0200

    Only show exception message for simple errors in the shell where a full traceback would be pointless

--- backend/main/views.py
+++ backend/main/views.py
@@ -95,10 +95,17 @@ class API:
 
         output_parts = result["output_parts"]
         if not result["awaiting_input"]:
-            if result["traceback_info"]:
-                output_parts.append(
-                    dict(isTraceback=True, data=result["traceback_info"])
-                )
+            traceback_info = result["traceback_info"]
+            if traceback_info:
+                if source == "shell" and len(traceback_info) == 1 and len(traceback_info[0]["frames"]) == 1:
+                    exception = traceback_info[0]["exception"]
+                    output_parts.append(
+                        dict(text=f"{exception['type']}: {exception['message']}\n", color="red")
+                    )
+                else:
+                    output_parts.append(
+                        dict(isTraceback=True, data=traceback_info)
+                    )
             output_parts.append(dict(text=">>> ", color="white"))
 
         birdseye_url = None
