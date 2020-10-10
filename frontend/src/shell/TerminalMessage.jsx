e622c42b5c668ab626971828a2fe963777b98d8c
Alex Hall <alex.mojaki@gmail.com>
2020-10-10 21:04:11 +0200

    Whitespace

--- frontend/src/shell/TerminalMessage.jsx
+++ frontend/src/shell/TerminalMessage.jsx
@@ -17,45 +17,45 @@ export default class TerminalMessage extends Component {
         </div>
         {
           content.data.map((traceback, tracebackIndex) =>
-              <div className="traceback" key={tracebackIndex}>
-                {
-                  traceback.frames.map((frame, frameIndex) =>
-                    <div className="traceback-frame" key={frameIndex}>
-                      <div className="traceback-frame-name">{frame.name}:</div>
-                      <table className="traceback-lines-table">
-                        <tbody>
-                        {
-                      frame.lines.map(line =>
-                        <tr key={line.lineno}>
-                          <td className="traceback-lineno">{line.lineno}</td>
-                          <td className="traceback-line-content codehilite"
-                              dangerouslySetInnerHTML={{__html: line.content}}/>
-                        </tr>
-                      )
-                    }
-                    </tbody>
-                  </table>
-                  <table className="traceback-variables-table">
-                    <tbody>
-                    {
-                      frame.variables.map((variable, variableIndex) =>
-                        <tr key={variableIndex}>
-                          <td className="traceback-variable-name codehilite"
-                              dangerouslySetInnerHTML={{__html: variable.name}}/>
-                          <td className="traceback-variable-value codehilite"
-                              dangerouslySetInnerHTML={{__html: variable.value}}/>
-                        </tr>
-                      )
-                    }
-                    </tbody>
-                  </table>
-                </div>
-              )
-            }
-            <div className="traceback-exception">
-              <strong>{traceback.exception.type}: </strong>{traceback.exception.message}
+            <div className="traceback" key={tracebackIndex}>
+              {
+                traceback.frames.map((frame, frameIndex) =>
+                  <div className="traceback-frame" key={frameIndex}>
+                    <div className="traceback-frame-name">{frame.name}:</div>
+                    <table className="traceback-lines-table">
+                      <tbody>
+                      {
+                        frame.lines.map(line =>
+                          <tr key={line.lineno}>
+                            <td className="traceback-lineno">{line.lineno}</td>
+                            <td className="traceback-line-content codehilite"
+                                dangerouslySetInnerHTML={{__html: line.content}}/>
+                          </tr>
+                        )
+                      }
+                      </tbody>
+                    </table>
+                    <table className="traceback-variables-table">
+                      <tbody>
+                      {
+                        frame.variables.map((variable, variableIndex) =>
+                          <tr key={variableIndex}>
+                            <td className="traceback-variable-name codehilite"
+                                dangerouslySetInnerHTML={{__html: variable.name}}/>
+                            <td className="traceback-variable-value codehilite"
+                                dangerouslySetInnerHTML={{__html: variable.value}}/>
+                          </tr>
+                        )
+                      }
+                      </tbody>
+                    </table>
+                  </div>
+                )
+              }
+              <div className="traceback-exception">
+                <strong>{traceback.exception.type}: </strong>{traceback.exception.message}
+              </div>
             </div>
-          </div>
           )
         }
       </div>
