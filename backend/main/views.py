7484390b22b58c123d557172a9df347ee2bec4fb
Alex Hall <alex.mojaki@gmail.com>
2020-10-11 11:47:51 +0200

    Show didyoumean suggestions in shell

--- backend/main/views.py
+++ backend/main/views.py
@@ -97,7 +97,12 @@ class API:
         if not result["awaiting_input"]:
             traceback_info = result["traceback_info"]
             if traceback_info:
-                if source == "shell" and len(traceback_info) == 1 and len(traceback_info[0]["frames"]) == 1:
+                if (
+                    source == "shell"
+                    and len(traceback_info) == 1
+                    and len(traceback_info[0]["frames"]) == 1
+                    and not traceback_info[0]["didyoumean"]
+                ):
                     exception = traceback_info[0]["exception"]
                     output_parts.append(
                         dict(text=f"{exception['type']}: {exception['message']}\n", color="red")
