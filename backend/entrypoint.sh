8e7385b8542e647991d8c0bc0fd3cdcffd1c8670
Alex Hall <alex.mojaki@gmail.com>
2021-02-23 12:41:49 +0200

    Try removing weird .wh files in entrypoint instead

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -eux
 
+rm -f /usr/lib/python3/dist-packages/.wh*
+
 # We probably don't want this to be automatic but it makes life a lot easier
 # For setting up the cloud
 python manage.py migrate
