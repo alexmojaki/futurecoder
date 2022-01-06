902d03a0a9d714000a5d8407b5ac57bddf9356b9
Alex Hall <alex.mojaki@gmail.com>
2022-01-06 22:09:13 +0200

    assert chapter titles and special messages are translated

--- core/translation.py
+++ core/translation.py
@@ -44,8 +44,7 @@ def get(msgid, default):
 
     result = translation.gettext(msgid)
     if result == msgid:
-        # TODO chapters and special messages should be translated
-        assert msgid.startswith(("code_bits.", "chapters.")) or "output_prediction_choices" in msgid or ".special_messages." in msgid
+        assert msgid.startswith(("code_bits.")) or "output_prediction_choices" in msgid
         return default
 
     def replace(match):
