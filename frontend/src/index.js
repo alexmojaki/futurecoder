898fe5766396a808923da2c89e433677a79a6806
Alex Hall <alex.mojaki@gmail.com>
2022-01-29 12:41:18 +0200

    Remove "Browser not supported" message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,19 +5,6 @@ import {Provider} from "react-redux";
 import {store} from "./store";
 
 ReactDOM.render(
-  typeof SharedArrayBuffer == "undefined" ?
-    <div className="container">
-      <h1>Browser not supported</h1>
-      <p>Sorry, futurecoder doesn't work yet on this browser. The following browsers should work:</p>
-      <ul>
-        <li>Chrome</li>
-        <li>Firefox</li>
-        <li>Edge</li>
-        <li>Opera</li>
-      </ul>
-      <p>Mobile devices and Safari are currently not supported.</p>
-    </div>
-    :
     <Provider store={store}>
       <App/>
     </Provider>,
