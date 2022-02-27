c4886b51b764ef7114ea1381a7e0cc43e7f97ec7
Michael Floering <michaelfloering@gmail.com>
2022-02-27 00:33:24 -0800

    WIP - trying PWA with vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,8 @@ import {defineConfig} from 'vite';
 // import react from '@vitejs/plugin-react';
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
+import envCompatible from 'vite-plugin-env-compatible'
+
 
 import { ManifestOptions, VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 
@@ -10,6 +12,9 @@ import { ManifestOptions, VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     reactRefresh(),
+    envCompatible({
+      prefix: "REACT_APP"
+    })
     // VitePWA({
     //   workbox: {
     //     importScripts: [
