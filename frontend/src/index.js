0ade9303f749f1e2cb34f77a6ed7ef5575959b76
Michael Floering <michaelfloering@gmail.com>
2022-02-26 18:34:23 -0800

    WIP - trying PWA... Not working yet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,15 @@ import {App} from './App';
 import {Provider} from "react-redux";
 import {store} from "./store";
 
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+
 ReactDOM.render(
     <Provider store={store}>
       <App/>
