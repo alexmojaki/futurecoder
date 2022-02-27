4cd88686b8e4f013b6909bcf15e3db84685ad04a
Michael Floering <michaelfloering@gmail.com>
2022-02-26 19:36:12 -0800

    Revert "Try cra-append-sw ( https://github.com/tszarzynski/cra-append-sw )"
    This reverts commit 5b42c949913a8e9fe670a5b19f6bbbcd393103aa.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,11 @@ import {store} from "./store";
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 
 ReactDOM.render(
     <Provider store={store}>
@@ -14,8 +19,3 @@ ReactDOM.render(
     </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
