5b42c949913a8e9fe670a5b19f6bbbcd393103aa
Michael Floering <michaelfloering@gmail.com>
2022-02-26 19:36:08 -0800

    Try cra-append-sw ( https://github.com/tszarzynski/cra-append-sw )

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,6 @@ import {store} from "./store";
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
-
 
 ReactDOM.render(
     <Provider store={store}>
@@ -19,3 +14,8 @@ ReactDOM.render(
     </Provider>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
