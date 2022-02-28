f595e946c87c0c25792179a76b1f0d3f934b9ea8
Michael Floering <michaelfloering@gmail.com>
2022-02-27 19:01:57 -0800

    This commit gets PWA mostly working.
    Just not offline-ready for some of the /static assets,
    so maybe that can be figured out next...

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,19 @@ import {App} from './App';
 import {Provider} from "react-redux";
 import {store} from "./store";
 
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+
 ReactDOM.render(
     <Provider store={store}>
       <App/>
     </Provider>,
   document.getElementById("root")
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
