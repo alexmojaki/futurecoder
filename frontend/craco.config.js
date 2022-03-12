97b5ffc48efb445b4d5e9e766d9df0c4be88a5f4
Michael Floering <michaelfloering@gmail.com>
2022-03-12 15:18:59 -0800

    Memo- how to make craco output to different dir

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,8 +1,17 @@
+const path = require('path');
+
 module.exports = {
   devServer: {
     headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp"
+      'Cross-Origin-Opener-Policy'  : 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
     },
-  }
-}
+  },
+  // XXX: Here's how to make craco output to ./course instead of ./build.
+  // webpack  : {
+  //   configure: (webpackConfig, {env, paths}) => {
+  //     paths.appBuild = webpackConfig.output.path = path.resolve('course');
+  //     return webpackConfig;
+  //   },
+  // },
+};
