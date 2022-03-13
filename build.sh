70af2b30c890bb7cc020682b4ace83af9c9ec46f
Michael Floering <michaelfloering@gmail.com>
2022-03-12 22:59:24 -0800

    build.sh - add var to skip a step

--- build.sh
+++ build.sh
@@ -5,8 +5,16 @@ set -eux
 rm -rf dist || true
 mkdir -p dist/course/
 
-poetry run python -m translations.generate_po_file
-poetry run python -m core.generate_static_files
+if [[ -z "${SKIP_TRANSLATIONS:-}" ]]; then
+    poetry run python -m translations.generate_po_file
+    poetry run python -m core.generate_static_files
+else
+    # If SKIP_TRANSLATIONS is set, set a default language for this build
+    if [[ -z "${FUTURECODER_LANGUAGE:-}" ]]; then
+        export FUTURECODER_LANGUAGE="en"
+    fi
+fi
+
 
 cd frontend
 REACT_APP_LANGUAGE=$FUTURECODER_LANGUAGE CI=false npm run build
