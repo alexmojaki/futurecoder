f1f0e522a3814cb29c7c582c76aef92578015d12
Michael Floering <michaelfloering@gmail.com>
2022-03-13 01:15:39 -0800

    (clean - code format in craco config file)

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -3,12 +3,12 @@ const path = require('path');
 module.exports = {
   devServer: {
     headers: {
-      'Cross-Origin-Opener-Policy'  : 'same-origin',
+      'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
   // XXX: Here's how to make craco output to ./course instead of ./build.
-  webpack  : {
+  webpack: {
     configure: (webpackConfig, {env, paths}) => {
       paths.appBuild = webpackConfig.output.path = path.resolve('course');
       return webpackConfig;
