a4702cf290197f0bf0425418b42e3cdf84d6e169
Michael Floering <michaelfloering@gmail.com>
2022-03-15 15:35:04 -0700

    Remove extraneous comment

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -1,18 +1,4 @@
-/* eslint-disable */
-// Otherwise webpack can fail silently
-// https://github.com/facebook/create-react-app/issues/8014
-
-// This optional code is used to register a service worker.
-// register() is not called by default.
-
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs open on the page have been closed, since previously cached
-// resources are updated in the background.
-
-// To learn more about the benefits of this model and instructions on how to
-// opt-in, read https://cra.link/PWA
+// Reference: https://cra.link/PWA
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
