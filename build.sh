d43f8c0116396fc2d2d546363728689dea266134
Michael Floering <michaelfloering@gmail.com>
2022-03-15 17:25:15 -0700

    PR feedback - build.sh

--- build.sh
+++ build.sh
@@ -5,16 +5,8 @@ set -eux
 rm -rf dist || true
 mkdir -p dist/course/
 
-if [[ -z "${SKIP_TRANSLATIONS:-}" ]]; then
-    poetry run python -m translations.generate_po_file
-    poetry run python -m core.generate_static_files
-else
-    # If SKIP_TRANSLATIONS is set, set a default language for this build
-    if [[ -z "${FUTURECODER_LANGUAGE:-}" ]]; then
-        export FUTURECODER_LANGUAGE="en"
-    fi
-fi
-
+poetry run python -m translations.generate_po_file
+poetry run python -m core.generate_static_files
 
 cd frontend
 NODE_ENV=production REACT_APP_LANGUAGE=$FUTURECODER_LANGUAGE CI=false npm run build
