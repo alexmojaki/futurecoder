d93096cd998b6ab01365e4ba288516dbcda69743
Michael Floering <michaelfloering@gmail.com>
2022-03-17 13:12:21 -0700

    PR feedback - service-worker.js - wrap ALL registerRoutes in the NODE_ENV=production check

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -21,37 +21,34 @@ if (process.env.NODE_ENV === 'production') {
   // This variable (self.__WB_MANIFEST) must be present somewhere in your service worker file,
   // even if you decide not to use precaching. See https://cra.link/PWA
   precacheAndRoute(self.__WB_MANIFEST);
-}
 
-// "Set up App Shell-style routing. Learn more at
-// ... https://developers.google.com/web/fundamentals/architecture/app-shell "
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
-registerRoute(
-  // "Return false to exempt requests from being fulfilled by index.html."
-  ({request, url}) => {
-    // "If this isn't a navigation, skip."
-    if (request.mode !== 'navigate') {
-      return false;
-    }
+  // "Set up App Shell-style routing. Learn more at
+  // ... https://developers.google.com/web/fundamentals/architecture/app-shell "
+  const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+  registerRoute(
+    // "Return false to exempt requests from being fulfilled by index.html."
+    ({request, url}) => {
+      // "If this isn't a navigation, skip."
+      if (request.mode !== 'navigate') {
+        return false;
+      }
 
-    // "If this is a URL that starts with /_, skip."
-    if (url.pathname.startsWith('/_')) {
-      return false;
-    }
+      // "If this is a URL that starts with /_, skip."
+      if (url.pathname.startsWith('/_')) {
+        return false;
+      }
 
-    // "If this looks like a URL for a resource, because it contains // a file extension, skip."
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    }
+      // "If this looks like a URL for a resource, because it contains // a file extension, skip."
+      if (url.pathname.match(fileExtensionRegexp)) {
+        return false;
+      }
 
-    // "Return true to signal that we want to use the handler."
-    return true;
-  },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html'),
-);
+      // "Return true to signal that we want to use the handler."
+      return true;
+    },
+    createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html'),
+  );
 
-if (process.env.NODE_ENV === 'production') {
-// Application-specific routes for FutureCoder
   registerRoute(
     ({url}) => {
       const urlString = url.toString();
@@ -59,16 +56,19 @@ if (process.env.NODE_ENV === 'production') {
         urlString.startsWith('https://cdn.jsdelivr.net/') // Pyodide
         || urlString.startsWith('https://pyodide-cdn2.iodide.io') // Only used when we are testing bleeding-edge pyodide
         || url.hostname.endsWith('futurecoder.io')
-        || url.hostname.includes("localhost")
-        || url.hostname.includes("127.0.0.1")
+        || url.hostname.includes('localhost')
+        || url.hostname.includes('127.0.0.1')
       ) {
-        return true
+        return true;
       }
       return false;
     },
     new StaleWhileRevalidate({
       cacheName: 'everything',
       cacheableResponse: {statuses: [0, 200]},
+      // ^ Q: What's status 0 mean?
+      // ^ A: It's an opaque response, and it relates to third-party resources (and CORS).
+      // https://github.com/alexmojaki/futurecoder/pull/313/files/f5f0fded9f44fefd0bf44b99ee45f54a8badc272#r829408930
       plugins: [
         new ExpirationPlugin({maxEntries: 30}),
       ],
