102c069d608748288b1271715c0a1adfbf5a66ea
Michael Floering <michaelfloering@gmail.com>
2022-03-17 12:56:27 -0700

    tiny - remove an unused import

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -11,7 +11,7 @@ import {clientsClaim} from 'workbox-core';
 import {ExpirationPlugin} from 'workbox-expiration';
 import {precacheAndRoute, createHandlerBoundToURL} from 'workbox-precaching';
 import {registerRoute} from 'workbox-routing';
-import {NetworkFirst, StaleWhileRevalidate} from 'workbox-strategies';
+import {StaleWhileRevalidate} from 'workbox-strategies';
 
 clientsClaim();
 
