ea7a32d8763eea58deefbaf204512464ec901035
Michael Floering <michaelfloering@gmail.com>
2022-03-23 11:47:44 -0700

    Revert "Troubleshooting - try not-caching opaque responses"
    This reverts commit 09174d7c949183cf56c094805a6f4f39cba5caa8.

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -63,7 +63,10 @@ if (process.env.NODE_ENV === 'production') {
     },
     new StaleWhileRevalidate({
       cacheName: 'everything',
-      cacheableResponse: {statuses: [200]},
+      cacheableResponse: {statuses: [0, 200]},
+      // ^ Q: What's status 0 mean?
+      // ^ A: It's an opaque response, and it relates to third-party resources (and CORS).
+      // https://github.com/alexmojaki/futurecoder/pull/313/files/f5f0fded9f44fefd0bf44b99ee45f54a8badc272#r829408930
       plugins: [
         new ExpirationPlugin({maxEntries: 30}),
       ],
