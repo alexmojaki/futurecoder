6bdd917420919f0eccb6c00df13e7ec854c3457b
Michael Floering <michaelfloering@gmail.com>
2022-03-30 20:43:07 -0700

    Override WorkboxWebpackPlugin the CRACO way
    I did a search to see if/how other people are overriding
    WorkboxWebpackPlugin configuration in craco.config.js. Seems that the
    other error goes away when we do it inside {webpack.configure} instead of
    putting it in {plugins}.
    
    However... birdseye is not working as of this commit.
    So there are still things to troubleshoot...

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -9,32 +9,37 @@ module.exports = {
     configure: (webpackConfig, {env, paths}) => {
       // Output to ./course (instead of ./build)
       paths.appBuild = webpackConfig.output.path = path.resolve('course');
+
+      // Override config for WorkboxWebpackPlugin.{GenerateSW,InjectManifest}
+      webpackConfig.plugins = webpackConfig.plugins.map(plugin => {
+        if(plugin.constructor.name === 'InjectManifest') {
+          console.info({plugin})
+          return new WorkboxWebpackPlugin.InjectManifest({
+            // At time of writing, the {exclude,maximumFileSize*,dontCacheBust*} arguments are the same as after "CRA eject",
+            // and the swSrc/swDest arguments are (... or should be ...) equivalent.
+            swSrc: './src/service-worker.js',
+            swDest: 'service-worker.js',
+            dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
+            exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
+            maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // To get birdseye to be included in precaching ... This might work ...
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            include: [
+              // NOT SURE IF THIS IS ACTUALLY WORKING... Intention is to get all contents of "./public/",
+              // including birdseye stuff and bootstrap stuff, to be part of `precacheAndRoute(self.__WB_MANIFEST)`
+              // in service-worker.js. At time of writing, other options are Craco's defaults for WorkboxWebpackPlugin.
+              /public\/.*/
+            ],
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+          })
+        }
+
+        return plugin
+      })
+
       return webpackConfig;
     },
-    plugins: [
-      isEnvProduction &&
-      new WorkboxWebpackPlugin.InjectManifest({
-        // At time of writing, the swSrc snd other arguments are the same as when doing "CRA eject",
-        // and so they SHOULD behave the same as when this whole block (`plugins`) is commented-out.
-        // However, for some reason, we're getting "Can't find self.__WB_MANIFEST in your SW source"
-        // even though `self.__WB_MANIFEST` clearly exists in src/service-worker.js, and it works
-        // when the default settings are left in place... Rather opaque error right now...
-        swSrc: path.resolve(__dirname, 'src/service-worker.js'),
-        dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
-        exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-        // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // // To get birdseye to be included in precaching ... This might work ...
-        // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // include: [
-        //   // NOT SURE IF THIS IS ACTUALLY WORKING... Intention is to get all contents of "./public/",
-        //   // including birdseye stuff and bootstrap stuff, to be part of `precacheAndRoute(self.__WB_MANIFEST)`
-        //   // in service-worker.js. At time of writing, other options are Craco's defaults for WorkboxWebpackPlugin.
-        //   /public\/.*/
-        // ],
-        // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-      }),
-    ],
   },
 
   // Staged for deletion -- the CORS headers override should not be needed as of recent commits in https://github.com/alexmojaki/futurecoder/pull/320
