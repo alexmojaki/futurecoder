df57e559e4e49b7df3c929c4a73d4605cd7cf6b8
Michael Floering <michaelfloering@gmail.com>
2022-03-31 08:36:53 -0700

    Remove app-shell style routing (it proved unnecessary)
    It was here from the boilerplate
    
    When I tried removing it before, I thought it broke something. So either
    that problem's gone, or there was not even a problem.
    
    That makes sense because as Alex said:
    
    > futurecoder uses `#` fragments for client-side routing, not paths.

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -24,38 +24,6 @@ if (process.env.NODE_ENV === 'production') {
   // even if you decide not to use precaching. See https://cra.link/PWA
   precacheAndRoute(self.__WB_MANIFEST);
 
-  // "Set up App Shell-style routing. Learn more at
-  // ... https://developers.google.com/web/fundamentals/architecture/app-shell "
-  const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
-  registerRoute(
-    // "Return false to exempt requests from being fulfilled by index.html."
-    ({request, url}) => {
-      // "If this isn't a navigation, skip."
-      if (request.mode !== 'navigate') {
-        return false;
-      }
-
-      // "If this is a URL that starts with /_, skip."
-      if (url.pathname.startsWith('/_')) {
-        return false;
-      }
-
-      // XXX don't use the app shell routing for birdseye!
-      if (url.pathname.includes('birdseye')) {
-        return false;
-      }
-
-      // "If this looks like a URL for a resource, because it contains // a file extension, skip."
-      if (url.pathname.match(fileExtensionRegexp)) {
-        return false;
-      }
-
-      // "Return true to signal that we want to use the handler."
-      return true;
-    },
-    createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html'),
-  );
-
   registerRoute(
     ({url}) => {
       const urlString = url.toString();
