0be6cb3970a07cf2e7830872a51bf32a477597d5
Michael Floering <michaelfloering@gmail.com>
2022-03-30 19:46:29 -0700

    WIP. Non-working. Try overriding craco's WebpackWorkboxPlugin.InjectManifest options.

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,12 +1,48 @@
 const path = require('path');
+const fs = require('fs');
+
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const paths = require('./paths'); // <-- Copied from "CRA eject" -- "config" folder.
+
+const isEnvProduction = process.env.NODE_ENV === "production"
+const swSrc = paths.swSrc;
+console.info({swSrc, exists: fs.existsSync(swSrc)})
 
 module.exports = {
-  // Output to ./course (instead of ./build)
   webpack: {
     configure: (webpackConfig, {env, paths}) => {
+      // Output to ./course (instead of ./build)
       paths.appBuild = webpackConfig.output.path = path.resolve('course');
       return webpackConfig;
     },
+    plugins: [
+      isEnvProduction &&
+      fs.existsSync(swSrc) &&
+      new WorkboxWebpackPlugin.InjectManifest({
+        // At time of writing, the swSrc snd other arguments are the same as when doing "CRA eject",
+        // and so they SHOULD behave the same as when this whole block (`plugins`) is commented-out.
+        // However, for some reason, we're getting "Can't find self.__WB_MANIFEST in your SW source"
+        // even though `self.__WB_MANIFEST` clearly exists in src/service-worker.js, and it works
+        // when the default settings are left in place... Rather opaque error right now...
+        swSrc,
+        dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
+        exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
+        // Bump up the default maximum size (2mb) that's precached,
+        // to make lazy-loading failure scenarios less likely.
+        // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // // To get birdseye to be included in precaching ... This might work ...
+        // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // include: [
+        //   // NOT SURE IF THIS IS ACTUALLY WORKING... Intention is to get all contents of "./public/",
+        //   // including birdseye stuff and bootstrap stuff, to be part of `precacheAndRoute(self.__WB_MANIFEST)`
+        //   // in service-worker.js. At time of writing, other options are Craco's defaults for WorkboxWebpackPlugin.
+        //   /public\/.*/
+        // ],
+        // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      }),
+    ],
   },
 
   // Staged for deletion -- the CORS headers override should not be needed as of recent commits in https://github.com/alexmojaki/futurecoder/pull/320
