25cbe1a4ea8e74dfb787e19f782415b53b9e6e2c
Michael Floering <michaelfloering@gmail.com>
2022-03-31 08:30:55 -0700

    Removing the CORS headers from the dev server
    Alex said:
    
    > First, remove the cross origin headers from the dev server in the
    craco config.

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -8,12 +8,4 @@ module.exports = {
       return webpackConfig;
     },
   },
-
-  // Staged for deletion -- the CORS headers override should not be needed as of recent commits in https://github.com/alexmojaki/futurecoder/pull/320
-  // devServer: {
-  //   headers: {
-  //     'Cross-Origin-Opener-Policy': 'same-origin',
-  //     'Cross-Origin-Embedder-Policy': 'require-corp',
-  //   },
-  // },
 };
