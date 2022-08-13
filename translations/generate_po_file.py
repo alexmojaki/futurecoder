f5e8b9ecbb40e2fe08585fcbe64c2bd5db0e7f9a
Alex Hall <alex.mojaki@gmail.com>
2022-08-13 14:13:27 +0200

    Check that indented __code__ never happens normally

--- translations/generate_po_file.py
+++ translations/generate_po_file.py
@@ -149,6 +149,7 @@ def text_entry(msgid, text, comments=()):
             code_bits[node_text].add(f"{search_link(msgid)}\n\n{code_text}")
             comments.add(search_link(t.code_bit(node_text)))
 
+    assert f" __code" not in text
     comment = "\n\n".join([page_link, *code_comments, *sorted(comments)])
     entry(msgid, text, comment)
 
