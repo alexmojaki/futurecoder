bed806bd684c99bf197c03ae18de818940f1ace9
Alex Hall <alex.mojaki@gmail.com>
2022-09-26 10:30:27 +0200

    newer friendly traceback, no need to explicitly ignore warnings

--- core/runner/friendly_traceback.py
+++ core/runner/friendly_traceback.py
@@ -1,6 +1,5 @@
 import sys
 import linecache
-import warnings
 
 from markdown import markdown
 
@@ -17,8 +16,6 @@ import friendly_traceback
 linecache.getlines = old_getlines  # undo friendly monkeypatching
 
 friendly_traceback.set_lang(t.current_language or "en")
-
-warnings.simplefilter("ignore")
 
 
 def friendly_message(e, double_newline: bool):
