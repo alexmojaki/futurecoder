5d5be10d0e36f666b494517ba2072e5f1875538a
Alex Hall <alex.mojaki@gmail.com>
2023-01-28 18:30:05 +0200

    just the logo

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -4,5 +4,7 @@ set -eux
 
 # Run tests in CI, i.e. GitHub Actions
 
+export DISPLAY=:99
+chromedriver --url-base=/wd/hub &
 npm install -g firebase-tools
 firebase emulators:exec "poetry run pytest tests"
