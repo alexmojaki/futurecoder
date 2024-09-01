0c107337b528d0be6f39b22eea3a1c2c1589a8e7
Alex Hall <alex.mojaki@gmail.com>
2024-09-01 17:22:08 +0200

    Update pyodide and pyodide-worker-runner

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const {PyodidePlugin} = require("@pyodide/webpack-plugin");
 
 module.exports = {
-  // Output to ./course (instead of ./build)
   webpack: {
+    plugins: {
+      add: [
+        new PyodidePlugin(),
+      ]
+    },
+    // Output to ./course (instead of ./build)
     configure: (webpackConfig, {env, paths}) => {
       paths.appBuild = webpackConfig.output.path = path.resolve('course');
       return webpackConfig;
