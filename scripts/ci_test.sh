0836d9b885b1ee6e54896301ef3c149d4ac95a23
Alex Hall <alex.mojaki@gmail.com>
2026-01-10 20:33:04 +0200

    limit firebase-tools to fix CI

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -6,5 +6,5 @@ set -eux
 
 export DISPLAY=:99
 chromedriver --url-base=/wd/hub &
-npm install -g firebase-tools
+npm install -g 'firebase-tools@<15.0.0'
 firebase emulators:exec "poetry run pytest tests"
